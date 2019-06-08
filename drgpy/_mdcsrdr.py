b6623d83e599498b952471adca4f32e92b4fdc1b
yubin-park <yubin.park@gmail.com>
2019-06-08 07:02:54 -0400

    mdcs reader cleaned

--- drgpy/_mdcsrdr.py
+++ drgpy/_mdcsrdr.py
@@ -3,6 +3,9 @@ import re
 from pkg_resources import resource_filename as rscfn
 from collections import defaultdict
 
+dx_pttrn = "[A-TV-Z][0-9][0-9AB][0-9A-TV-Z]{0,4}"
+pr_pttrn = "[A-HJ-NP-Z0-9]{7}"
+
 def shorten(x):
     x = " ".join(x.split())
     x = x.replace("DIAGNOSES", "DIAGNOSIS")
@@ -20,120 +23,155 @@ def get_codetype(dx_lst, pr_lst, cache):
     elif len(dx_lst) == 0 and len(pr_lst) > 0:
         codetype = "pr"
     else:
-        if "diagnos" in cache["sh2"].lower():
+        if "diagnos" in cache["D"].lower():
             codetype = "dx"
-        elif "procedure" in cache["sh2"].lower():
+        elif "procedure" in cache["D"].lower():
             codetype = "pr"
     return codetype
 
 def get_label(cache):
-    i = len(cache["_"][cache["sh1"]])
-    return "{}|{}.{}".format(cache["sh1"], i, cache["sh2"])
-
-def update_cache(level, cache):
-    if cache["sh1"] != "" and cache["sh2"] != "":
-        cache["_"][cache["sh1"]][cache["sh2"]] = 1
-    if level[:2] == "SH":
-        cache["sh2"] = ""
-    if level == "SH1":
-        cache["sh1"] = ""
+    return "{}|{}".format(cache["C"], cache["D"])
 
 def update_mapping(dxmap, prmap, cache):
-    if len(cache["sc"]) > 0:
-        codetype = cache["sc"][0]
-        code = cache["sc"][1]
+    if len(cache["E"]) > 0:
+        codetype = cache["E"][0]
+        code = cache["E"][1]
         label = get_label(cache)
-        if len(cache["sc"]) > 2:
-            label = "{}|{}".format(label, cache["sc"][2]) # additional code
+        if len(cache["E"]) > 2:
+            label = "{}|{}".format(label, cache["E"][2]) # additional code
         if codetype == "dx":
             dxmap[code].append(label)
         elif codetype == "pr":
             prmap[code].append(label)
-        cache["sc"] = []
- 
-def parse(line, cursor, dxmap, prmap, cache):
+        cache["E"] = []
 
-    # sh1
+def is_A(line, cursor):
+    return (line[1:3] == "==")
+
+def parse_A(line, cursor, dxmap, cache):
+    mdc_lst = re.findall("MDC\s(\d{2})\s", line)
+    dx_lst = re.findall("\s{2}" + dx_pttrn + "\s+", line)
+    if len(mdc_lst) > 0:
+        cache["A"] = "MDC" + mdc_lst[0]
+    elif len(dx_lst) > 0:
+        dx = dx_lst[0].strip()
+        dxmap[dx].append(cache["A"])
+
+def is_B(line, cursor):
+    return (cursor in {"A", "B", "E"} and 
+            (line[:2] == "+-" or line[0]=="|"))
+
+def is_C(line, cursor):
+    return (cursor in {"B", "C", "E"} and line[:4] == "DRG ")
+
+def parse_C(line, cursor, cache, _cursor):
+
     drg_lst = re.findall("DRG\s(\d{3})\s", line)
-    dx_pttrn = "[A-TV-Z][0-9][0-9AB][0-9A-TV-Z]{0,4}"
-    pr_pttrn = "[A-HJ-NP-Z0-9]{7}"
-    dx_lst = re.findall("\s{2}"+dx_pttrn + "\s+", line)
-    pr_lst = re.findall("\s{2}"+pr_pttrn + "\*?\s+", line)
-    pr_lst += re.findall("\s{3}and\s"+pr_pttrn + "\*?\s+", line)
-
-    if len(drg_lst) > 0 and line[:3] == "DRG": # SH1
+    if len(drg_lst) > 0:
         drg = drg_lst[0]
-        if cursor != "SH1":
-            update_mapping(dxmap, prmap, cache)
-            update_cache("SH1", cache)
-            cache["sh1"] = drg
+        if _cursor != "C":
+            cache["C"] = drg
         else:
-            cache["sh1"] += ("&" + drg)
-        cursor = "SH1"
-    elif len(dx_lst) == 0 and len(pr_lst) == 0: # SH2
-        if cursor != "SH2":
-            update_mapping(dxmap, prmap, cache)
-            update_cache("SH2", cache)
-            cache["sh2"] = shorten(line.strip())
-        else:
-            cache["sh2"] += (" " + shorten(line.strip()))
-        cursor = "SH2"
-    else: # SC: code sections
-        codetype = get_codetype(dx_lst, pr_lst, cache)
-        tokens = line.split()
-        code = tokens[0]
-        if tokens[0] == "and":
-            code = tokens[1]
-            cache["sc"].append(code)
-        else:
-            if len(cache["sc"]) > 0:
-                update_mapping(dxmap, prmap, cache)
-            cache["sc"] = [codetype, code]
-        cursor = "SC"
+            cache["C"] += ("&" + drg)
 
-    return cursor
+def is_D(line, cursor):
+    return (cursor in {"C", "D", "E"} and line[:2].strip() != "")
+
+def parse_D(line, cursor, cache, _cursor):
+
+    line = line.strip()
+    if line == "":
+        return
+
+    if _cursor != "D":
+        cache["D"] = shorten(line)
+    else:
+        cache["D"] += (" " + shorten(line))
+
+def is_E(line, cursor):
+    return (cursor in {"D", "E"} and line[:2] == "  ")
+
+def parse_E(line, cursor, dxmap, prmap, cache, _cursor):
+
+    line = line.strip() 
+    if line == "":
+        update_mapping(dxmap, prmap, cache)
+        return
+
+    dx_lst = re.findall(dx_pttrn + "\s+", line)
+    pr_lst = re.findall(pr_pttrn + "\*?\s+", line)
+    pr_lst += re.findall("and\s"+pr_pttrn + "\*?\s+", line)
+    codetype = get_codetype(dx_lst, pr_lst, cache)
+    tokens = [x.replace("*", "") for x in line.split()]
+    code = tokens[0]
+    if tokens[0] == "and":
+        code = tokens[1]
+        cache["E"].append(code)
+    else:
+        if len(cache["E"]) > 0:
+            update_mapping(dxmap, prmap, cache)
+        cache["E"] = [codetype, code]
+
 
 def read(fn, dxmap, prmap):
 
     """
-    M        :?=======...                 
-    M        MDC XX...
-    M        :?=======...
-    M          MDC contents
-    (blank) 
-    A        +-------...
-    A        | Abstract Table about DRG...
-    A        +-------...
-    (blank)  
-    SH1      DRG XXX...
-    (blank)
-    SH2      Details...
-    SC         Codes...
+    A        :?=======...                 
+    A        MDC XX...
+    A        :?=======...
+    A          MDC contents
+    A        (blank) 
+    B        +-------...
+    B        | Abstract Table about DRG...
+    B        +-------...
+    B        (blank)  
+    C        DRG XXX...
+    C        (blank)
+    D        Details...
+    D        (blank)
+    E          Codes...
 
-    * e.g. M -> A -> Sx -> Sx -> A -> Sx -> M
-    * Sx is always followed by either A or Sx
-    * M always precedes A
-    * SH2 
+    *pattern: A -> [B -> [C -> [D -> E]]]
     """
-    cursor = ""
-    cache = {"sh1": "", 
-            "sh2": "", 
-            "sc": "", 
+
+    _cursor = "F"
+    cursor = "F"
+    cache = {"A": "", 
+            "C": "", 
+            "D": "",
+            "E": [],
             "_": defaultdict(dict)}
     fn = rscfn(__name__, fn)
    
     with open(fn, "r") as fp:
         for line in fp:
             line = line.replace("\n","")
-            if line.strip() == "":
-                continue
 
-            if line[1:3] == "==":
-                cursor = "M"
-            elif line[:2] == "+-" or line[0]=="|":
+            if line.strip()=="":
+                pass
+            elif is_A(line, cursor):
                 cursor = "A"
-            elif cursor != "M": 
-                cursor = parse(line, cursor, dxmap, prmap, cache) 
+            elif is_B(line, cursor):
+                cursor = "B"
+            elif is_C(line, cursor):
+                cursor = "C"
+            elif is_D(line, cursor):
+                cursor = "D"
+            elif is_E(line, cursor):
+                cursor = "E"
+
+            if cursor == "A":
+                parse_A(line, cursor, dxmap, cache)            
+            elif cursor == "B":
+                pass
+            elif cursor == "C":
+                parse_C(line, cursor, cache, _cursor) 
+            elif cursor == "D":
+                parse_D(line, cursor, cache, _cursor) 
+            elif cursor == "E":
+                parse_E(line, cursor, dxmap, prmap, cache, _cursor)
+
+            _cursor = cursor
 
     return dxmap, prmap
                 
@@ -145,7 +183,7 @@ if __name__=="__main__":
                 "data/mdcs_22_25.txt"]
     dxmap = defaultdict(list)
     prmap = defaultdict(list)
-    for fn in fn_lst[1:2]:
+    for fn in fn_lst[:1]:
         dxmap, prmap = read(fn, dxmap, prmap)
 
     import json
--- tests/msdrg_tests.py
+++ tests/msdrg_tests.py
@@ -9,6 +9,8 @@ class TestDRGEngine(unittest.TestCase):
         print(el_lst)
         el_lst = de.get_dxel(["", "A021"])
         print(el_lst)
+        el_lst = de.get_dxel(["G40B19"])
+        print(el_lst)
 
         el_lst = de.get_prel(["02YA0Z0", "02YA0Z1"])
         print(el_lst)
