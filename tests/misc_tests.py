6873b90a0c69af014e8a2858606cd2baeca0abe9
yubin-park <yubin.park@gmail.com>
2022-04-18 21:25:57 -0400

    untracked change

---
+++ tests/misc_tests.py
@@ -0,0 +1,19 @@
+import unittest
+from drgpy.msdrg import DRGEngine
+
+class TestMisc(unittest.TestCase):
+
+    def test_misc(self):
+
+        de = DRGEngine()
+
+        drg_lst = de.get_drg(['O99324', 'F1120', 'B1920', 'O9842', 'O701', 'F1210', 'Z370', 'Z3A40'],  ['3E033VJ', '4A1HX4Z', '10E0XZZ', '10907ZC'])
+        print(drg_lst)
+
+
+if __name__=="__main__":
+    unittest.main()
+
+
+
+
